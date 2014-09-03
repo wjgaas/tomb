3ed2b438a9ba6767ddd5f228c134b5546a62c4ca
dirtysalt <dirtysalt1987@gmail.com>
2014-09-03 17:48:58 +0800

    fix typo

--- py/torrent/btclean.sh
+++ py/torrent/btclean.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 if [[ $# -lt 1 ]]; then
-  echo "Wrong # of params"
-  echo "Usage:   batch_clean.sh dir prefix start_index [options]"
-  echo "         prefix use today (yyyy-MM-dd) as default"
-  echo "         start_index's default is 0"
-  echo "Example: batch_clean.sh /tmp AV- 0"
-  echo "         batch_clean.sh /tmp"
+  echo "Usage:   btclean.sh input-dir output-prefix index [options]"
+  echo "         output-prefix: default 'today'(yyyy-MM-dd)"
+  echo "         index: default 0"
+  echo "Example: btclean.sh /tmp AV- 0"
   exit 1
 fi
 
@@ -13,9 +11,9 @@ today=`date +%Y-%m-%d`
 files=$1/*.torrent; shift 1
 prefix=${1:-$today}; shift 1
 index=${1:-0}; shift 1
+
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-
 for f in $files; do
   cmd="./cleanser.py -i $f -o $prefix-$index $@"
   echo "$cmd"
@@ -25,4 +23,3 @@ done
 IFS=$SAVEIFS
 
 echo "job done !"
-
