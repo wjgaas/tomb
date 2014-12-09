31a065e9e82275c9c163ffa0e0003dab74ce36ab
dirtysalt <dirtysalt1987@gmail.com>
2014-12-09 12:31:45 +0800

    update leetcode

---
+++ leetcode/find-minimum-in-rotated-sorted-array-ii.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding:utf-8
+#Copyright (C) dirlt
+
+class Solution:
+    # @param num, a list of integer
+    # @return an integer
+    def findMin(self, num):
+        return min(num)
