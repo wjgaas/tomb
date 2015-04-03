ea4653b9cdab697a0d34f6352f342aebc95b7f3c
dirtysalt <dirtysalt1987@gmail.com>
2015-04-03 22:47:39 +0800

    update prototxt

--- kaggle/digit-recognizer/caffe-visual.py
+++ kaggle/digit-recognizer/caffe-visual.py
@@ -7,35 +7,47 @@ import sys
 sys.path.append(os.path.join(os.environ['HOME'], 'repo/caffe/python'))
 import caffe
 
-caffe.set_mode_cpu()
-net = caffe.Net('caffe-conf/test.prototxt',
-                'uv_iter_2000.caffemodel',
-                caffe.TEST)
+import caffe.draw
+from caffe.proto import caffe_pb2
+from google.protobuf import text_format
 
-from common import *
-data = read_test(10)
-start_timer()
-data = data.reshape((-1, 1, 28, 28))
-out = net.forward_all(**{'data': data})
-blobs = net._blobs
-for name in net._layer_names: print name
+net = caffe_pb2.NetParameter()
+text_format.Merge(open('caffe-conf/train.prototxt').read(), net)
+caffe.draw.draw_net_to_file(net, 'train.jpg', 'LR')
 
-import pylab as plt
-import matplotlib.cm as cm
+net = caffe_pb2.NetParameter()
+text_format.Merge(open('caffe-conf/test.prototxt').read(), net)
+caffe.draw.draw_net_to_file(net, 'test.jpg', 'LR')
 
-def simple_plot(data, r0, c0):
-    fig, axs = plt.subplots(r0, c0)
-    for r in range(0, r0):
-        for c in range(0, c0):
-            ax = axs[r][c]
-            ax.axes.get_xaxis().set_visible(False)
-            ax.axes.get_yaxis().set_visible(False)
-            im = data[r * c0 + c]
-            ax.imshow(im, cmap = cm.Greys_r)
-    plt.show()
+# caffe.set_mode_cpu()
+# net = caffe.Net('caffe-conf/test.prototxt',
+#                 'uv_iter_2000.caffemodel',
+#                 caffe.TEST)
 
-data = blobs[1].data[2] # conv1, 3th instance.
-simple_plot(data, 4, 8)
+# from common import *
+# data = read_test(10)
+# start_timer()
+# data = data.reshape((-1, 1, 28, 28))
+# out = net.forward_all(**{'data': data})
+# blobs = net._blobs
+# for name in net._layer_names: print name
 
-data = blobs[3].data[2] # conv2, 3th instance.
-simple_plot(data, 8, 8)
+# import pylab as plt
+# import matplotlib.cm as cm
+
+# def simple_plot(data, r0, c0):
+#     fig, axs = plt.subplots(r0, c0)
+#     for r in range(0, r0):
+#         for c in range(0, c0):
+#             ax = axs[r][c]
+#             ax.axes.get_xaxis().set_visible(False)
+#             ax.axes.get_yaxis().set_visible(False)
+#             im = data[r * c0 + c]
+#             ax.imshow(im, cmap = cm.Greys_r)
+#     plt.show()
+
+# data = blobs[1].data[2] # conv1, 3th instance.
+# simple_plot(data, 4, 8)
+
+# data = blobs[3].data[2] # conv2, 3th instance.
+# simple_plot(data, 8, 8)
