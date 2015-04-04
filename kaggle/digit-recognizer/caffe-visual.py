76ebf43c30b17915c16c7055e9767c4b901bc98a
dirtysalt <dirtysalt1987@gmail.com>
2015-04-04 12:50:22 +0800

    fix visual

--- kaggle/digit-recognizer/caffe-visual.py
+++ kaggle/digit-recognizer/caffe-visual.py
@@ -11,13 +11,15 @@ import caffe.draw
 from caffe.proto import caffe_pb2
 from google.protobuf import text_format
 
+print 'train...'
 net = caffe_pb2.NetParameter()
 text_format.Merge(open('caffe-conf/train.prototxt').read(), net)
-caffe.draw.draw_net_to_file(net, 'train.jpg', 'TB')
+caffe.draw.draw_net_to_file(net, 'caffe-train.png', 'TB')
 
+print 'test...'
 net = caffe_pb2.NetParameter()
 text_format.Merge(open('caffe-conf/test.prototxt').read(), net)
-caffe.draw.draw_net_to_file(net, 'test.jpg', 'TB')
+caffe.draw.draw_net_to_file(net, 'caffe-test.png', 'TB')
 
 caffe.set_mode_cpu()
 net = caffe.Net('caffe-conf/test.prototxt',
@@ -35,7 +37,7 @@ for name in net._layer_names: print name
 import pylab as plt
 import matplotlib.cm as cm
 
-def simple_plot(data, r0, c0):
+def simple_plot(data, r0, c0, fname):
     fig, axs = plt.subplots(r0, c0)
     for r in range(0, r0):
         for c in range(0, c0):
@@ -44,23 +46,22 @@ def simple_plot(data, r0, c0):
             ax.axes.get_yaxis().set_visible(False)
             im = data[r * c0 + c]
             ax.imshow(im, cmap = cm.Greys_r)
-    plt.show()
+    if fname: plt.savefig(fname, dpi = 72)
+    else: plt.show()
 
 nth = 2 # nth instance
 print 'conv1...'
 data = blobs[1].data[nth] # conv1
-simple_plot(data, 4, 8)
+simple_plot(data, 4, 8, 'caffe-conv1.png')
 
 print 'pool1...'
 data = blobs[2].data[nth] # pool1
-simple_plot(data, 4, 8)
+simple_plot(data, 4, 8, 'caffe-pool1.png')
 
 print 'conv2...'
 data = blobs[3].data[nth] # conv2
-simple_plot(data, 8, 8)
+simple_plot(data, 8, 8, 'caffe-conv2.png')
 
 print 'pool2...'
 data = blobs[4].data[nth] # pool2
-simple_plot(data, 8, 8)
-
-
+simple_plot(data, 8, 8, 'caffe-pool2.png')
