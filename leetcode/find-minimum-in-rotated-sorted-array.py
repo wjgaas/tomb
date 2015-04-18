61a61668901e75fbbfd11d1a8e81a5da42b91c97
dirtysalt <dirtysalt1987@gmail.com>
2015-04-18 23:37:54 +0800

    remove leetcode

--- leetcode/find-minimum-in-rotated-sorted-array.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
-
-class Solution:
-    # @param num, a list of integer
-    # @return an integer
-    def findMin(self, num):
-        return min(num)
--- leetcode/sort-colors.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-#Copyright (C) dirlt
-
-class Solution:
-    # @param A a list of integers
-    # @return nothing, sort in place
-    def sortColors(self, A):
-        A.sort()
